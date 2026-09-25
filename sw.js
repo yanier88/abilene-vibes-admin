f31ef909fdf389c469f81edf7a6b934e4f6fe7de
yanier88 <yanier88@users.noreply.github.com>
2026-09-25 02:07:22 +0000

    deploy: yanier88/abilene-vibes@9d54e5155fb6f995ec9faa8bae06e6461e0e26b9

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "abilene-vibes-v23";
+const BASE_URL = new URL(self.registration.scope).pathname;
+const APP_SHELL = [
+  BASE_URL,
+  `${BASE_URL}manifest.webmanifest`,
+  `${BASE_URL}icon-192.png`,
+  `${BASE_URL}icon-512.png`,
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data?.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match(BASE_URL))),
+  );
+});
